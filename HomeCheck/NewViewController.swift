2267f4857687f3ec1b837f0ad5644aa3c7977271
HristinaBuyanova <hristina-a@mail.ru>
2023-09-04 15:37:40 +0300

    Add struct post and color view

--- HomeCheck/NewViewController.swift
+++ HomeCheck/NewViewController.swift
@@ -14,16 +14,9 @@ class NewViewController: UIViewController {
 
         // Do any additional setup after loading the view.
     }
-    
 
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    func setupView() {
+        view.backgroundColor = .systemGray
     }
-    */
 
 }
