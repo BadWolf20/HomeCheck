e4a19c018215340e614209dcd7a068e4053b041d
HristinaBuyanova <hristina-a@mail.ru>
2023-09-04 15:37:40 +0300

    Add struct post and color view

--- HomeCheck/Post.swift
+++ HomeCheck/Post.swift
@@ -5,4 +5,14 @@
 //  Created by Христина Буянова on 04.09.2023.
 //
 
-import Foundation
+import UIKit
+
+struct PostNew {
+    var author: String
+    var description: String
+    var image: String
+    var likes: Int
+    var views: Int
+}
+
+
